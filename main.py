1eeda6180cc6d03e09ac426249ebbf430ab6735e
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-11-11 17:29:23 +0900

    main.py now uses generated csv and separates sets appropriately

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import glob
 import pandas as pd
 import xml.etree.ElementTree as ET
+from sklearn.model_selection import train_test_split
 
 from tflite_model_maker.config import ExportFormat
 from tflite_model_maker import model_spec
@@ -22,23 +23,49 @@ labelDict = {'r':'puck_red','o':'puck_off', 'f':'robot_front', 'br':'robot_backr
 
 def xml_to_csv(path):
     xml_list = []
-    for xml_file in glob.glob(path + '/*.xml'):
+    globlist = glob.glob(path + '/*.xml')
+    for xml_file in globlist:
         tree = ET.parse(xml_file)
         root = tree.getroot()
-        for member in root.findall('object'):
-            value = ("UNASSIGNED",
-                'image_set/' + root.find('filename').text,
-                labelDict.get(member[0].text),
-                int(member[4][0].text),
-                int(member[4][1].text),
-                None,
-                None,
-                int(member[4][2].text),
-                int(member[4][3].text),
-                None,
-                None,
-                )
-            xml_list.append(value)
+        boundingBoxes = root.findall('object')
+
+        if (len(boundingBoxes) == 0):
+            # value = ("TRAINING",
+            #          'image_set/' + root.find('filename').text,
+            #          None,
+            #          None,
+            #          None,
+            #          None,
+            #          None,
+            #          None,
+            #          None,
+            #          None,
+            #          None,
+            #          )
+            # xml_list.append(value)
+            pass
+        else:
+            for idx, member in enumerate(boundingBoxes):
+                if idx > int(0.7 * len(boundingBoxes)):
+                    set = "TRAINING"
+                elif idx > int(0.8 * len(boundingBoxes)):
+                    set = "VALIDATION"
+                else:
+                    set = "TEST"
+
+                value = (set,
+                    'image_set/' + root.find('filename').text,
+                    labelDict.get(member[0].text),
+                    int(member[4][0].text),
+                    int(member[4][1].text),
+                    None,
+                    None,
+                    int(member[4][2].text),
+                    int(member[4][3].text),
+                    None,
+                    None,
+                    )
+                xml_list.append(value)
     column_name = ['set', 'filename', 'class', 'xmin', 'ymin', None, None, 'xmax', 'ymax', None, None]
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     return xml_df
@@ -46,37 +73,6 @@ def xml_to_csv(path):
 spec = model_spec.get('efficientdet_lite0')
 
 print("started loading data set")
-
-# split data into training and testing set
-# if os.path.exists('./train/images'):
-#   shutil.rmtree('./train/images')
-# if os.path.exists('./train/annotations'):
-#   shutil.rmtree('./train/annotations')
-# if os.path.exists('./test/images'):
-#   shutil.rmtree('./test/images')
-# if os.path.exists('./test/annotations'):
-#   shutil.rmtree('./test/annotations')
-#
-# os.makedirs('./train/images')
-# os.makedirs('./train/annotations')
-# os.makedirs('./test/images')
-# os.makedirs('./test/annotations')
-#
-# image_paths = os.listdir('./image_set')
-# random.shuffle(image_paths)
-#
-# for i, image_path in enumerate(image_paths):
-#   if i < int(len(image_paths) * 0.8):
-#     if os.path.exists(f'./xml_dataset/{image_path.replace("jpg", "xml")}'): # Not all images have objects in them
-#       shutil.copy(f'./image_set/{image_path}', './train/images')
-#       shutil.copy(f'./xml_dataset/{image_path.replace("jpg", "xml")}', './train/annotations')
-#   else:
-#     if os.path.exists(f'./xml_dataset/{image_path.replace("jpg", "xml")}'):
-#       shutil.copy(f'./image_set/{image_path}', './test/images')
-#       shutil.copy(f'./xml_dataset/{image_path.replace("jpg", "xml")}', './test/annotations')
-#
-# train_data = object_detector.DataLoader.from_pascal_voc('./train/images', './train/annotations', ['r', 'o', 'f', 'br','bl'])
-# test_data = object_detector.DataLoader.from_pascal_voc('./test/images', './test/annotations', ['r', 'o', 'f', 'br','bl'])
 
 image_path = os.path.join(os.getcwd(), 'xml_dataset')
 xml_df = xml_to_csv(image_path)
@@ -88,9 +84,9 @@ train_data, validation_data, test_data = object_detector.DataLoader.from_csv('./
 print("loaded data set...creating model")
 
 model = object_detector.create(train_data, model_spec=spec, epochs=1, batch_size=1, train_whole_model=True, validation_data=validation_data)
-print("created model...evaluating model")
+print("created model.")
 
-model.evaluate(test_data)
+# model.evaluate(test_data)
 print("exporting model")
 
 model.export(export_dir='.')
